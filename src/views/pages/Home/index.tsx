a0bfa001fb0d755a7272441f7c5cf74315ab6333
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-04-28 14:44:43 +0100

    feat: Added tailwindcss

--- src/views/pages/Home/index.tsx
+++ src/views/pages/Home/index.tsx
@@ -1,67 +1,10 @@
-import { useEffect, useState } from "react";
-import Hero from "./_sections/Hero/Hero";
-
-import { getTrending } from "@/services/apis/themoviedb/requests/Trending";
-
-import Section from "atoms/Section";
-import Container from "atoms/Container";
-
-import TitleHeader from "molecules/TitleHeader";
-import PosterList from "molecules/Poster/PosterList";
-
-
 function Home() {
-    const [trending, setTrending] = useState({})
-
-    async function fetch() {
-        const res = await getTrending();
-        setTrending({...res, results: res.results.slice(0,7)})
-    }
-
-    useEffect(() => {
-        fetch()
-    }, [])
-
     return (
-        <div className="bg-[#071520]">
-            <Hero />
-
-            <Section id="trending" className="banner">
-            <Container>
-
-                <TitleHeader 
-                    title="🔥 Trending This Week"
-                    viewAllLink="movies" 
-                />
-                <PosterList data={trending.results} />
-
-            </Container>
-            </Section>
-
-            <Section id="latest">
-            <Container>
-
-                <TitleHeader 
-                    title="🎬 Coming up next"
-                    viewAllLink="movies" 
-                />
-                <PosterList data={trending.results} />
-            
-            </Container>
-            </Section>
-
-            <Section id="top-rated">
-            <Container>
-
-                <TitleHeader 
-                    title="⭐ Top Rated"
-                    viewAllLink="movies" 
-                />
-            
-            </Container>
-            </Section>
+        <div>
+            Home
         </div>
     )
 }
+
 
 export default Home;
