9688799d974d68531c354cc5f6255a3e87e05841
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-04-29 09:06:14 +0100

    chore: Added fake stock companies info

---
+++ src/database/fakeStockCompanies.tsx
@@ -0,0 +1,192 @@
+export const fakeStockCompanies = [
+    {
+        symbol: "AAPL",
+        name: "Apple Inc.",
+        sector: "Technology",
+        industry: "Consumer Electronics",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "AMZN",
+        name: "Amazon.com Inc.",
+        sector: "Consumer Cyclical",
+        industry: "Internet Retail",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "GOOGL",
+        name: "Alphabet Inc.",
+        sector: "Communication Services",
+        industry: "Internet Content & Information",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "FB",
+        name: "Meta Platforms Inc.",
+        sector: "Communication Services",
+        industry: "Internet Content & Information",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "MSFT",
+        name: "Microsoft Corporation",
+        sector: "Technology",
+        industry: "Software-Infrastructure",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "NVDA",
+        name: "NVIDIA Corporation",
+        sector: "Technology",
+        industry: "Semiconductor Equipment & Materials",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "TSLA",
+        name: "Tesla, Inc.",
+        sector: "Consumer Cyclical",
+        industry: "Auto Manufacturers",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "ADBE",
+        name: "Adobe Inc.",
+        sector: "Technology",
+        industry: "Software-Application",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "BABA",
+        name: "Alibaba Group Holding Limited",
+        sector: "Consumer Cyclical",
+        industry: "Internet Retail",
+        exchange: "NYSE"
+    },
+    {
+        symbol: "BRK.A",
+        name: "Berkshire Hathaway Inc.",
+        sector: "Financial Services",
+        industry: "Insurance-Diversified",
+        exchange: "NYSE"
+    },
+    {
+        symbol: "CMCSA",
+        name: "Comcast Corporation",
+        sector: "Communication Services",
+        industry: "Pay TV",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "NFLX",
+        name: "Netflix, Inc.",
+        sector: "Communication Services",
+        industry: "Internet Content & Information",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "NKE",
+        name: "NIKE, Inc.",
+        sector: "Consumer Cyclical",
+        industry: "Footwear & Accessories",
+        exchange: "NYSE"
+    },
+    {
+        symbol: "PYPL",
+        name: "PayPal Holdings, Inc.",
+        sector: "Technology",
+        industry: "Payment Services",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "SHOP",
+        name: "Shopify Inc.",
+        sector: "Technology",
+        industry: "Software-Application",
+        exchange: "NYSE"
+    },
+    {
+        symbol: "V",
+        name: "Visa Inc.",
+        sector: "Technology",
+        industry: "Payment Services",
+        exchange: "NYSE"
+    },
+    {
+        symbol: "ZM",
+        name: "Zoom Video Communications, Inc.",
+        sector: "Technology",
+        industry: "Software-Application",
+        exchange: "NASDAQ"
+    },
+    {
+        symbol: "GOOG",
+        name: "Alphabet Inc.",
+        sector: "Communication Services",
+        industry: "Internet Content & Information",
+        exchange: "NASDAQ"
+      },
+      {
+        symbol: "JNJ",
+        name: "Johnson & Johnson",
+        sector: "Healthcare",
+        industry: "Drug Manufacturers-General",
+        exchange: "NYSE"
+      },
+      {
+        symbol: "JPM",
+        name: "JPMorgan Chase & Co.",
+        sector: "Financial Services",
+        industry: "Banks-Diversified",
+        exchange: "NYSE"
+      },
+      {
+        symbol: "KO",
+        name: "The Coca-Cola Company",
+        sector: "Consumer Defensive",
+        industry: "Beverages-Non-Alcoholic",
+        exchange: "NYSE"
+      },
+      {
+        symbol: "MCD",
+        name: "McDonald's Corporation",
+        sector: "Consumer Cyclical",
+        industry: "Restaurants",
+        exchange: "NYSE"
+      },
+      {
+        symbol: "MRK",
+        name: "Merck & Co., Inc.",
+        sector: "Healthcare",
+        industry: "Drug Manufacturers-General",
+        exchange: "NYSE"
+      },
+      {
+        symbol: "PG",
+        name: "The Procter & Gamble Company",
+        sector: "Consumer Defensive",
+        industry: "Household & Personal Products",
+        exchange: "NYSE"
+      },
+      {
+        symbol: "T",
+        name: "AT&T Inc.",
+        sector: "Communication Services",
+        industry: "Telecom Services",
+        exchange: "NYSE"
+      },
+      {
+        symbol: "TMUS",
+        name: "T-Mobile US, Inc.",
+        sector: "Communication Services",
+        industry: "Telecom Services",
+        exchange: "NASDAQ"
+      },
+      {
+        symbol: "VZ",
+        name: "Verizon Communications Inc.",
+        sector: "Communication Services",
+        industry: "Telecom Services",
+        exchange: "NYSE"
+      }
+]
+
