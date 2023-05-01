66878311fec8a008ac67fd678139060bf3554210
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-01 13:55:56 +0100

    fix: nav highithed color

--- src/views/pages/Dashboard/Notebook/NoteNavigation/NoteItem.tsx
+++ src/views/pages/Dashboard/Notebook/NoteNavigation/NoteItem.tsx
@@ -3,7 +3,7 @@ import dateHelper from "@/utils/dateHelper";
 function NoteItem({ item, isActive, onClick }:any) {
 
     return (
-        <li onClick={onClick} className={` ${item.id === isActive.id ? "bg-gray-100/20" : ""} w-full bg-white hover:bg-gray-300/10 border border-transparent hover:border-skin-brand-100/20 px-4 `}>
+        <li onClick={onClick} className={` ${item.id === isActive.id ? "bg-gray-100/20" : "bg-white"} w-full  hover:bg-gray-300/10 border border-transparent hover:border-skin-brand-100/20 px-4 `}>
         <button type="button" className="w-full h-full px-4 py-8 text-left  border-b-2 border-dashed border-gray-200">
         <div className="flex flex-col">
             <span>{dateHelper.formatDate(item.createdAt)}</span>
