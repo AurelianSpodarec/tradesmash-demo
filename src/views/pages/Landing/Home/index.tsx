20e584d46fc0e184325e7d8e07ab1498051e0e2f
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-01 13:53:24 +0100

    update: landing page minor details

--- src/views/pages/Landing/Home/index.tsx
+++ src/views/pages/Landing/Home/index.tsx
@@ -99,7 +99,7 @@ function Home() {
 
 
         
-            <section className="bg-skin-brand-500">
+            <section className="bg-blue-600">
             <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
 
                 <div className="mx-auto max-w-2xl text-center">
@@ -108,14 +108,14 @@ function Home() {
                     <br />
                     Start using our app today.
                 </h2>
-                <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-800">
+                <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-white">
                     Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur
                     commodo do ea.
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                     <a
                     href="#"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-md bg-skin-brand-600/70 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-skin-brand-500/70 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-skin-brand-600/70"
                     >
                     Get started
                     </a>
--- src/views/pages/Landing/_components/LayoutMain/_components/Header/Header.tsx
+++ src/views/pages/Landing/_components/LayoutMain/_components/Header/Header.tsx
@@ -17,7 +17,7 @@ function Header() {
             {/* <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                 <a href="javascript:void(0)" className="mr-5 hover:text-gray-900">First Link</a>
             </nav> */}
-            <Link to="/dashboard" className="inline-flex items-center bg-blue-500 border-0 py-1.5 px-4 focus:outline-none hover:bg-gray-200 rounded-lg text-white mt-4 md:mt-0">
+            <Link to="/dashboard" className="inline-flex items-center bg-skin-brand-500 border-0 py-1.5 px-4 focus:outline-none hover:bg-gray-200 rounded-lg text-white mt-4 md:mt-0">
                 Get Started
             </Link>
             </div>
