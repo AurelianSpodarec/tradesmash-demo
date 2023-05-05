1bc7ab893597dae05342fc96ed76b9dd3f3b841a
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-05 08:38:27 +0100

    fix: TS errors in tradeSliceSelectors

--- src/store/features/trades/tradeSliceSelectors.tsx
+++ src/store/features/trades/tradeSliceSelectors.tsx
@@ -7,46 +7,48 @@ export const hasTradeNotes = (id: number) => useSelector((state: { notes:any }) 
 
 export const getTotalTradesGrossPnL = () => {
     return useSelector((state: { trades: TradeState }) => {
-      const total = state.trades.trades.reduce((acc, trade) => {
-        return acc + trade.grossProfitAndLoss;
-      }, 0);
-      return currencyFormatter.format(total);
+        const total = state.trades.trades.reduce((acc, trade) => {
+            return acc + (trade.grossProfitAndLoss ?? 0);
+        }, 0);
+        
+        return currencyFormatter.format(total);
     });
-  };
-
-
+};
 
 export const getBiggestProfit = () => useSelector((state: { trades: TradeState }) => {
-    const profits = state.trades.trades.filter(trade => trade.grossProfitAndLoss > 0).map(trade => trade.grossProfitAndLoss);
-    if (profits.length === 0) return null; // no profits found
+    const profits = state.trades.trades.filter(trade => trade.grossProfitAndLoss != null && trade.grossProfitAndLoss > 0).map(trade => trade.grossProfitAndLoss ?? 0);
+    if (profits.length === 0) return currencyFormatter.format(0); // no profits found
     
-    const biggestProfit = profits.reduce((a, b) => Math.max(a, b));
+    const biggestProfit = profits.reduce((a, b) => Math.max(a ?? 0, b ?? 0));
     return currencyFormatter.format(biggestProfit);
 });
 
+
 export const getBiggestLoss = () => useSelector((state: { trades: TradeState }) => {
-    const losses = state.trades.trades.filter(trade => trade.grossProfitAndLoss < 0).map(trade => trade.grossProfitAndLoss);
-    if (losses.length === 0) return 0; // no losses found
+    const losses = state.trades.trades.filter(trade => trade.grossProfitAndLoss != null && trade.grossProfitAndLoss < 0).map(trade => trade.grossProfitAndLoss ?? 0);
+    if (losses.length === 0) return currencyFormatter.format(0); // no losses found
     
     const biggestLoss = losses.reduce((a, b) => Math.min(a, b));
     return currencyFormatter.format(biggestLoss);
 });
 
+
 export const getTotalSwingGrossPL = () => useSelector((state: {trades: TradeState}) => {
     const total = state.trades.trades
         .filter(trade => trade.strategy === 'swing trading')
-        .reduce((total, trade) => total + trade.grossProfitAndLoss, 0);
+        .reduce((total, trade) => total + (trade.grossProfitAndLoss ?? 0), 0);
 
     return currencyFormatter.format(total)
 });
 
-export const getTotalScalpGrossPL = () => useSelector((state) => {
-    const total =  state.trades.trades.
-        filter(trade => trade.strategy === 'scalp')
-        .reduce((total, trade) => total + trade.grossProfitAndLoss, 0);
+export const getTotalScalpGrossPL = () => useSelector((state: { trades: TradeState} ) => {
+    const total =  state.trades.trades
+        .filter(trade => trade.strategy === 'scalping')
+        .reduce((total, trade) => total + (trade.grossProfitAndLoss ?? 0), 0);
 
     return currencyFormatter.format(total)
 });
+
 
 
 export const getProfitLossRatio = () => useSelector((state: { trades: TradeState }) => {
@@ -54,10 +56,14 @@ export const getProfitLossRatio = () => useSelector((state: { trades: TradeState
     let totalLoss = 0;
 
     for (const trade of state.trades.trades) {
-        if (trade.grossProfitAndLoss > 0) {
-            totalProfit += trade.grossProfitAndLoss;
-        } else {
-            totalLoss += trade.grossProfitAndLoss;
+        const grossProfitAndLoss = trade.grossProfitAndLoss;
+     
+        if (grossProfitAndLoss != null) {
+            if (grossProfitAndLoss > 0) {
+                totalProfit += grossProfitAndLoss;
+            } else {
+                totalLoss += grossProfitAndLoss;
+            }
         }
     }
 
